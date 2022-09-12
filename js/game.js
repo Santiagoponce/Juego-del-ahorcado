f724f279d21708b5b07e541390debabcda4c1ca0
Santiago <slponce@fi.uba.ar>
2022-09-12 05:12:48 -0300

    Challenge Alura nro.2

---
+++ js/game.js
@@ -0,0 +1,196 @@
+//Codigo de juego
+
+function randomWord(vector)
+{
+    if(!vector.length)
+    {
+       return -1;
+    }
+
+    let index=Math.floor(Math.random()*(vector.length));
+    return vector[index];
+
+}
+
+function launchBoard(game)
+{
+    var maskedWord=document.querySelector(".maskedWord");
+    for(let i=0; i<game.word.length; i++)
+    {
+        let li=document.createElement("li");
+        li.classList.add("boardLetter");
+        maskedWord.append(li);
+
+    }
+
+    game.nodeList=document.querySelectorAll(".boardLetter");
+    return 0;
+
+}
+
+function importLevels(pathJsonFileLevels)
+{
+    var levels;
+    var xhr= new XMLHttpRequest();
+    
+    xhr.addEventListener("load",function ()
+    {
+        levels=JSON.parse(xhr.responseText);
+    }
+    );
+
+    xhr.open("GET",pathJsonFileLevels,false);
+    xhr.send();
+    return levels;
+
+}
+function newGame(levels)
+{
+    var game={
+        word:randomWord(levels).secretWord.toUpperCase(),
+        attemps:10,
+        miss:0,
+        hits:0,
+        isGameOver:false,
+        usedLetters:'',
+        nodeList:[]
+        
+        }
+    return game;
+}
+
+function newLevel(string)
+{
+    var level={
+        secretWord:string
+    };
+
+return level;
+}
+
+function tryOut(letter,game)
+{
+    let temp;
+    letter=letter.toUpperCase();
+    if(game.isGameOver!=false)
+    {        return -1;
+    }   
+    if(count(letter,game.usedLetters))
+    {
+        return 0;
+    }
+
+        game.usedLetters+=letter;
+
+
+    if(!(temp=count(letter,game.word)))
+        {
+            game.attemps--;
+            displayHangman(game);
+            game.miss++;
+        }
+    else
+    {
+        unmaskLetter(letter,game);
+        game.hits+=temp;
+    }
+
+
+    checkStatus(game);
+}
+
+function unmaskLetter(letter,game){
+    for(let i=0;i<game.word.length;i++)
+    {
+        if(game.word[i]==letter)
+            game.nodeList[i].textContent=letter;
+    }
+
+}
+
+function checkStatus(game){
+    
+    if(game.hits==(game.word).length)
+    {   
+        var gameOverScreen=gameOverLayer.querySelector("#gameOverScreen");
+        gameOverScreen.textContent="GANASTE";
+        game.isGameOver=true;
+        show(gameOverLayer);
+        hide(gameLayer);
+
+
+
+        return 0;
+    }
+
+    else if(!game.attemps)
+    {
+        
+        game.isGameOver=true;
+
+        
+        var gameOverScreen=gameOverLayer.querySelector("#gameOverScreen");
+        gameOverScreen.textContent="PERDISTE";
+        game.isGameOver=true;
+        show(gameOverLayer);
+        
+        hide(gameLayer);
+        return 0;
+    }
+
+
+}
+function count(c,string)
+{
+    c=c.toUpperCase();
+    let i,occurrences;
+
+    for(i=0,occurrences=0;i<string.length;i++)
+        if(string[i]==c)
+            occurrences++;
+    
+
+    return occurrences;
+
+}
+
+
+function finishGame(game)
+{
+    game.nodeList.forEach(function (li)
+    {
+        li.remove();
+    });
+     hideHangman(hangman);
+
+return 0;
+}
+
+function refreshKeyBoard(event)
+{
+    tryOut(this.value,gameInstance);
+    event.target.value='';
+
+}
+
+
+
+
+function displayHangman(game)
+{
+    show(hangman[game.miss]);
+}
+function hideHangman(hangman)
+{
+    hangman.forEach(function (value)
+    {
+        hide(value);
+    });
+}
+
+var levels=importLevels("JSON/levels.json");
+var hiddenInput=document.querySelector("#hiddenInput");
+hiddenInput.addEventListener("input",refreshKeyBoard);
+var hangman=document.querySelectorAll(".hangman");
+hideHangman(hangman);
+var gameInstance;
---
+++ js/transitions.js
@@ -0,0 +1,109 @@
+
+var initButton=document.querySelector("#initGameButton");
+var addWordButton=document.querySelector("#addWordButton");
+
+
+var saveAndStartButton=document.querySelector("#saveAndStartButton");
+var cancelButton=document.querySelector("#cancelButton");
+
+
+var newGameButton=document.querySelector("#newGameButton");
+var surrenderButton=document.querySelector("#surrenderButton");
+var newWordInput=document.querySelector(".newWordInput");
+var exitButton=document.querySelector("#exitButton");
+
+var homeLayer=document.querySelector("#homeLayer");
+var gameLayer=document.querySelector("#gameLayer");
+var addWordLayer=document.querySelector("#addWordLayer");
+var gameOverLayer=document.querySelector("#gameOverLayer");
+var html=document.querySelector("html");
+html.addEventListener("click",function(){hiddenInput.focus();
+})
+
+
+
+function hide(element)
+{
+    element.classList.add("hide");
+}
+
+function makeInvisible(element)
+{
+    element.classList.add("invisible");
+}
+function show(element)
+{
+    element.classList.remove("hide");
+}
+
+
+
+initButton.addEventListener("click",initGame);
+
+function initGame()
+{
+    gameInstance =newGame(levels);
+    launchBoard(gameInstance);
+    hide(homeLayer);
+    show(gameLayer);
+    hiddenInput.focus();
+    makeInvisible(hiddenInput);
+
+
+}
+
+addWordButton.addEventListener("click",addWord);
+
+function addWord()
+{
+    hide(homeLayer);
+    show(addWordLayer);
+}
+
+cancelButton.addEventListener("click",cancel);
+
+function cancel()
+{
+    hide(addWordLayer);
+    show(homeLayer);
+    newWordInput.value='';
+}
+
+
+saveAndStartButton.addEventListener("click",saveAndStart);
+function saveAndStart(){
+    hide(addWordLayer);
+    show(gameLayer);
+    levels.push(newLevel(newWordInput.value));
+    newWordInput.value='';
+    initGame();
+
+
+}
+
+surrenderButton.addEventListener("click",surrender);
+function surrender(){
+    finishGame(gameInstance);
+    hide(gameLayer);
+    show(homeLayer);
+
+}
+
+newGameButton.addEventListener("click",newGame);
+function newGame()
+{
+    finishGame(gameInstance);
+    gameInstance =newGame(levels);
+    hiddenInput.focus();
+    launchBoard(gameInstance);
+}
+
+exitButton.addEventListener("click",exit);
+function exit()
+{
+    finishGame(gameInstance);
+    hide(gameOverLayer);
+    show(homeLayer);
+    return 0;
+}
+
